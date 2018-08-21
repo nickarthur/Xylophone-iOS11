ffb0cf5cccf44a20c69c5e05ff2be474bd9102a2
NickArthur <nickarthur.night@gmail.com>
2018-08-21 06:37:47 -0500

    got code working using AVAudio and AVAsset

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var audioPlayer: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-
-
-    @IBAction func notePressed(_ sender: UIButton) {
         
-        
-        
+        let defaultNote: URL = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        prepareToPlay(defaultNote)
     }
     
-  
+    @IBAction func notePressed(_ sender: UIButton) {
+        sender.showsTouchWhenHighlighted = true
 
+        guard let url = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav")
+            else {return}
+
+        audioPlayer?.replaceCurrentItem(with: AVPlayerItem(url: url))
+        audioPlayer?.playImmediately(atRate: 1.0)
+    }
+    
+    func prepareToPlay(_ url: URL) {
+        // Create asset to be played
+        let asset = AVAsset(url: url)
+        
+        let assetKeys = [
+            "playable",
+            "hasProtectedContent"
+        ]
+        // Create a new AVPlayerItem with the asset and an
+        // array of asset keys to be automatically loaded
+        let playerItem = AVPlayerItem(asset: asset,
+                                  automaticallyLoadedAssetKeys: assetKeys)
+        
+        
+        // Associate the player item with the player
+        audioPlayer = AVPlayer(playerItem: playerItem)
+    }
+    
 }
 
